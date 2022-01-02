28b96457b7977bfe213c24b8760a06b04bbcf1f5
MrCat47 <09ariaslopez@gmail.com>
2022-01-02 17:44:04 -0500

    second commit, passed

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -202,7 +202,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola \${nombre}!";
+  return "Hola " + nombre + "!";
 }
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
@@ -221,7 +221,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  return alto * ancho / 2;
+  return altura * base / 2;
 }
 
 
@@ -239,17 +239,27 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra == ["a","e","i","0","u"]){
-    if (letra.length > 1){
-      return "Dato incorrecto";
-    }else{
+  if (letra.length == 1){
+    if (letra == "a"){
       return "Es vocal";
     }
-  }else{
-    return "Dato incorrecto";
-  }
+    if (letra == "e"){
+      return "Es vocal";
+    }
+    if (letra == "i"){
+      return "Es vocal";
+    }
+    if (letra == "o"){
+      return "Es vocal";
+    }
+    if (letra == "u"){
+      return "Es vocal";
+    }else{
+      return "Dato incorrecto";
+    }
+  }else{return "Dato incorrecto";
 }
-
+}
 
 
 // No modificar nada debajo de esta línea
